401e207fa0d37e981b8dd79aa3f7d2744840a6b6
Engid <ngideo@gmail.com>
2018-03-12 03:12:23 -0500

    adding notes and toys

--- src/StepSeq.js
+++ src/StepSeq.js
@@ -32,6 +32,48 @@ const Explanation = () => {
 
  export default class StepSeq extends Component {
 
+  constructor(props){
+    super(props);
+
+    var keys = new Tone.Players({
+    "A" : "./audio/casio/A1.[mp3|ogg]",
+    "C#" : "./audio/casio/Cs2.[mp3|ogg]",
+    "E" : "./audio/casio/E2.[mp3|ogg]",
+    "F#" : "./audio/casio/Fs2.[mp3|ogg]",
+    }, {
+    "volume" : -10,
+    "fadeOut" : "64n",
+    }).toMaster();
+
+    //the notes
+    var noteNames = ["F#", "E", "C#", "A"];
+
+      // Sequence set to 16 steps, each step an index number into the current column (or step). 
+      // The `col` param would usually be a 'note', but its used here to lookup the note stored 
+      // in a location that can be updated. When a Sequence is created, the Array of
+      // events passed in are fixed to the new Sequence (although they may be able to be swapped
+      // with new events). 
+    var loop = new Tone.Sequence(function(time, col){
+      var column = matrix1.matrix[col];
+      for (var i = 0; i < 4; i++){
+        if (column[i] === 1){
+          //slightly randomized velocities
+          var vel = Math.random() * 0.5 + 0.5;
+          keys.get(noteNames[i]).start(time, 0, "32n", 0, vel);
+        }
+      }
+    }, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "16n");
+
+    // If you'd rather not have to type out an array of values 0..N you could use this
+    // sexy one-liner:
+    // [...Array(16).keys()]
+    //Note the awesome use of the spread operator can be attributed to this guru:
+    // https://stackoverflow.com/a/36953272/4821960
+
+    Tone.Transport.start();
+
+  }
+
 
   render(){
     return (
